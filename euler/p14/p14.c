c490ae87e06cab3f93902e5487bb2dd0995c96cc
Ahmet Kartal <ahmetkartal@DESKTOP-N034PQV.localdomain>
2020-11-09 20:35:02 +0300

    Program Optimised

--- euler/p14/p14.c
+++ euler/p14/p14.c
@@ -7,52 +7,54 @@ n → 3n + 1 (n is odd)
 Using the rule above and starting with 13, we generate the following sequence:
 
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
+Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 
 Which starting number, under one million, produces the longest chain?
 
 NOTE: Once the chain starts the terms are allowed to go above one million.
 */
 
+
+#include<stdlib.h>
 #include<stdio.h>
 
-int main (void)
+int factory(long long x);
+
+int main(void)
 {
-    long chain = 0;
-    long number = 2;
-    long copy;
-    long maxChain = 0;
-    long maxNumber = 0;
-    long limit;
+    long long biggestNumber = 0;
+    int max = 0;
+    int number = 10;
 
-    printf("What's the uplimit: ");
-    scanf("%li", &limit);
-
-    for(int i = 0; i<limit; i++)
+    while(number < 10000000)
     {
-        copy = number;
-        while(number > 1)
+        int chain = factory(number);
+        if(chain > max)
         {
-            if((number%2) == 0)
-            {
-                number = number/2;
-                chain++;
-            }
-            else
-            {
-                number = 3*number + 1;
-                chain++;
-            }
+            max = chain;
+            biggestNumber = number;
         }
-        if(chain > maxChain)
-        {
-            maxChain = chain;
-            maxNumber = copy;
-        }
-        chain = 0;
-        number = copy + 1;
+        number++;
     }
+    printf("The longest chain is %d\nProduced by %lld\n", max, biggestNumber);
+}
 
-    printf("Under %li, the longest chain produced by: %li\n", limit, maxNumber);
-    printf("Max chain is: %li\n", maxChain);
+int factory(long long x)
+{
+    int chain = 0;
+
+    while(x > 1)
+    {
+        if((x % 2) == 0)
+        {
+            x /= 2;
+        }
+        else
+        {
+            x = 3*x + 1;
+        }
+        chain ++;
+    }
+    return chain;
 }
